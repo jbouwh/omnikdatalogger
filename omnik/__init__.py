fc4b61f844b60014383cdd4cf1e7a65f2ff6abce
Pascal Prins <pascal.prins@foobar-it.com>
2019-06-14 08:38:22 +0200

    Upped version

--- omnik/__init__.py
+++ omnik/__init__.py
@@ -8,7 +8,7 @@ from .datalogger import DataLogger
 
 logging.basicConfig(stream=sys.stdout, level=os.environ.get('LOGLEVEL', logging.INFO))
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 class RepeatedJob(object):
   def __init__(self, interval, function, *args, **kwargs):
