d553103152acdfd8b14f6be9e617f0fb42aa3fda
Jan Bouwhuis <jbouwh@hotmail.com>
2020-06-20 00:10:07 +0200

    Upadte version to v0.99.4

--- apps/omnikdatalogger/omnik/__init__.py
+++ apps/omnikdatalogger/omnik/__init__.py
@@ -9,7 +9,7 @@ import threading
 logging.basicConfig(stream=sys.stdout,
                     level=os.environ.get('LOGLEVEL', logging.INFO))
 
-__version__ = '0.99.1'
+__version__ = '0.99.2'
 
 logger = logging.getLogger(__name__)
 
