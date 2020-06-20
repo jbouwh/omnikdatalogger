2ff8ee23771678d71d161b906842dacde4b4b0b0
Jan Bouwhuis <jbouwh@hotmail.com>
2020-06-20 11:23:02 +0200

    Fix executable bit PyPi 0.99.14

--- apps/omnikdatalogger/omnik/__init__.py
+++ apps/omnikdatalogger/omnik/__init__.py
@@ -9,7 +9,7 @@ import threading
 logging.basicConfig(stream=sys.stdout,
                     level=os.environ.get('LOGLEVEL', logging.INFO))
 
-__version__ = '0.99.12'
+__version__ = '0.99.14'
 
 logger = logging.getLogger(__name__)
 
