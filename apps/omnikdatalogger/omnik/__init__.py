f6098211d72d357a4d139d923e0fe22ef71961d5
Jan Bouwhuis <jbouwh@hotmail.com>
2020-06-20 02:59:06 +0200

    0.99.11

--- apps/omnikdatalogger/omnik/__init__.py
+++ apps/omnikdatalogger/omnik/__init__.py
@@ -9,7 +9,7 @@ import threading
 logging.basicConfig(stream=sys.stdout,
                     level=os.environ.get('LOGLEVEL', logging.INFO))
 
-__version__ = '0.99.10'
+__version__ = '0.99.11'
 
 logger = logging.getLogger(__name__)
 
