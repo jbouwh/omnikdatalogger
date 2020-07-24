2a0daefa4edca00b7d7f52feaa4dcdc93101defb
Jan Bouwhuis <jbouwh@hotmail.com>
2020-07-24 17:15:16 +0200

    v1.0.4 for PyPi to update requirements

--- apps/omnikdatalogger/omnik/__init__.py
+++ apps/omnikdatalogger/omnik/__init__.py
@@ -9,7 +9,7 @@ import threading
 logging.basicConfig(stream=sys.stdout,
                     level=os.environ.get('LOGLEVEL', logging.INFO))
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 logger = logging.getLogger(__name__)
 
