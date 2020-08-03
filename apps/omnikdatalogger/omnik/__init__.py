e19a42a125a7e87da271c063277a354a9ab17cc3
Jan Bouwhuis <jbouwh@hotmail.com>
2020-08-04 01:12:57 +0200

    Update version to v1.2.12

--- apps/omnikdatalogger/omnik/__init__.py
+++ apps/omnikdatalogger/omnik/__init__.py
@@ -9,7 +9,7 @@ import threading
 logging.basicConfig(stream=sys.stdout,
                     level=os.environ.get('LOGLEVEL', logging.INFO))
 
-__version__ = '1.2.11'
+__version__ = '1.2.12'
 
 logger = logging.getLogger(__name__)
 
