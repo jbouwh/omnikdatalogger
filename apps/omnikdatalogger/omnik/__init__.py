8840c01a434a58ce8a685324d3cc72e9568fc7de
jbouwh <jan@jbsoft.nl>
2021-06-03 13:41:05 +0000

    devcontainer code checks

--- apps/omnikdatalogger/omnik/__init__.py
+++ apps/omnikdatalogger/omnik/__init__.py
@@ -9,7 +9,7 @@ import threading
 logging.basicConfig(stream=sys.stdout,
                     level=os.environ.get('LOGLEVEL', logging.INFO))
 
-__version__ = '1.4.4'
+__version__ = '1.5.0'
 
 logger = logging.getLogger(__name__)
 
