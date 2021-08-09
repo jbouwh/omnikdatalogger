f94be1a88c37931de4708cce4093c8855298d501
jbouwh <jan@jbsoft.nl>
2021-08-09 23:19:37 +0000

    correct condition to reset

--- apps/omnikdatalogger/omnik/datalogger.py
+++ apps/omnikdatalogger/omnik/datalogger.py
@@ -981,7 +981,7 @@ class DataLogger(object):
             self.cache[last_current_power] = current_power
             self.cache[last_reset] = last_reset_payload
             self._update_persistant_cache()
-        elif self.cache.get(last_reset) and datetime.fromisoformat(
+        elif not self.cache.get(last_reset) or datetime.fromisoformat(
             last_reset_payload
         ) > datetime.fromisoformat(self.cache.get(last_reset)):
             # reset daily counters and last_reset
