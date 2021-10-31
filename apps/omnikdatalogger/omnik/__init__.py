b5795bb790057bdca0f12052e716d0ae88421f6c
jbouwh <jan@jbsoft.nl>
2021-10-31 13:26:13 +0000

    Bump to public release v1.8.0

--- apps/omnikdatalogger/omnik/__init__.py
+++ apps/omnikdatalogger/omnik/__init__.py
@@ -8,7 +8,7 @@ import threading
 
 logging.basicConfig(stream=sys.stdout, level=os.environ.get("LOGLEVEL", logging.INFO))
 
-__version__ = "1.8.0-beta"
+__version__ = "1.8.0"
 
 logger = logging.getLogger(__name__)
 
