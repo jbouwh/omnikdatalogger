ca16efcc4436ae2f69a92fe86c726f19bbff0bfd
jbouwh <jan@jbsoft.nl>
2021-11-15 08:02:50 +0000

    disable pre-release publishing

--- apps/omnikdatalogger/omnik/__init__.py
+++ apps/omnikdatalogger/omnik/__init__.py
@@ -8,7 +8,7 @@ import threading
 
 logging.basicConfig(stream=sys.stdout, level=os.environ.get("LOGLEVEL", logging.INFO))
 
-__version__ = "1.9.0"
+__version__ = "1.10.0-beta-2"
 
 logger = logging.getLogger(__name__)
 
