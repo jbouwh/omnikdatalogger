09f17ca4b93ed15ee97ef7fd30e4136eb0b0e50d
jbouwh <jan@jbsoft.nl>
2022-02-21 19:00:33 +0000

    Fix version influx client

--- apps/omnikdatalogger/omnik/__init__.py
+++ apps/omnikdatalogger/omnik/__init__.py
@@ -8,7 +8,7 @@ import threading
 
 logging.basicConfig(stream=sys.stdout, level=os.environ.get("LOGLEVEL", logging.INFO))
 
-__version__ = "1.11.0"
+__version__ = "1.11.1"
 
 logger = logging.getLogger(__name__)
 
